27cfd9b83ee4efd6ef12355a13dc6326e058d332
Ivan Avdeev <me@provod.works>
2024-08-31 22:24:17 -0400

    enumerate more things

--- enum.c
+++ enum.c
@@ -4,77 +4,154 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <memory.h>
+#include <stdlib.h>
 
-#define MSG(fmt, ...) printf(fmt "\n", ##__VA_ARGS__)
+#define MSG(fmt, ...) fprintf(stderr, fmt "\n", ##__VA_ARGS__)
+#define MSGA(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
 
-void enumerateModeResources(int fd, const drmModeResPtr res) {
-	MSG("\tcount_fbs = %d", res->count_fbs);
-	for (int i = 0; i < res->count_fbs; ++i)
-		MSG("\t\t%d: 0x%x", i, res->fbs[i]);
-
-	MSG("\tcount_crtcs = %d", res->count_crtcs);
-	for (int i = 0; i < res->count_crtcs; ++i) {
-		MSG("\t\t%d: 0x%x", i, res->crtcs[i]);
-		drmModeCrtcPtr crtc = drmModeGetCrtc(fd, res->crtcs[i]);
-		if (crtc) {
-			MSG("\t\t\tbuffer_id = 0x%x gamma_size = %d", crtc->buffer_id, crtc->gamma_size);
-			MSG("\t\t\t(%u %u %u %u) %d",
-				crtc->x, crtc->y, crtc->width, crtc->height, crtc->mode_valid);
-			MSG("\t\t\tmode.name = %s", crtc->mode.name);
-			drmModeFreeCrtc(crtc);
-		}
-	}
-
-	MSG("\tcount_connectors = %d", res->count_connectors);
-	for (int i = 0; i < res->count_connectors; ++i) {
-		MSG("\t\t%d: 0x%x", i, res->connectors[i]);
-		drmModeConnectorPtr conn = drmModeGetConnectorCurrent(fd, res->connectors[i]);
-		if (conn) {
-			drmModeFreeConnector(conn);
-		}
-	}
-
-	MSG("\tcount_encoders = %d", res->count_encoders);
-	for (int i = 0; i < res->count_encoders; ++i)
-		MSG("\t\t%d: 0x%x", i, res->encoders[i]);
-
-	MSG("\twidth: %u .. %u", res->min_width, res->max_width);
-	MSG("\theight: %u .. %u", res->min_height, res->max_height);
+static void printDrmModeType(uint32_t type) {
+#define CHECK_MODE(mode) if ((type&(DRM_MODE_TYPE_##mode)) == (DRM_MODE_TYPE_##mode)) MSGA(#mode" ")
+	CHECK_MODE(BUILTIN);
+	CHECK_MODE(CLOCK_C);
+	CHECK_MODE(CRTC_C);
+	CHECK_MODE(PREFERRED);
+	CHECK_MODE(DEFAULT);
+	CHECK_MODE(USERDEF);
+	CHECK_MODE(DRIVER);
+#undef CHECK_MODE
 }
 
-int main(int argc, const char *argv[]) {
-	const int available = drmAvailable();
-	if (!available)
-		return 1;
+static void printDrmModeFlags(uint32_t flags) {
+#define CHECK_FLAG(flag) if ((flags&(DRM_MODE_FLAG_##flag)) == (DRM_MODE_FLAG_##flag)) MSGA(#flag" ")
+	CHECK_FLAG(PHSYNC);
+	CHECK_FLAG(NHSYNC);
+	CHECK_FLAG(PVSYNC);
+	CHECK_FLAG(NVSYNC);
+	CHECK_FLAG(INTERLACE);
+	CHECK_FLAG(DBLSCAN);
+	CHECK_FLAG(CSYNC);
+	CHECK_FLAG(PCSYNC);
+	CHECK_FLAG(NCSYNC);
+	CHECK_FLAG(HSKEW);
+	CHECK_FLAG(BCAST);
+	CHECK_FLAG(PIXMUX);
+	CHECK_FLAG(DBLCLK);
+	CHECK_FLAG(CLKDIV2);
+	CHECK_FLAG(3D_FRAME_PACKING);
+	CHECK_FLAG(3D_FIELD_ALTERNATIVE);
+	CHECK_FLAG(3D_LINE_ALTERNATIVE);
+	CHECK_FLAG(3D_SIDE_BY_SIDE_FULL);
+	CHECK_FLAG(3D_L_DEPTH);
+	CHECK_FLAG(3D_L_DEPTH_GFX_GFX_DEPTH);
+	CHECK_FLAG(3D_TOP_AND_BOTTOM);
+	CHECK_FLAG(3D_SIDE_BY_SIDE_HALF);
+	CHECK_FLAG(PIC_AR_4_3);
+	CHECK_FLAG(PIC_AR_16_9);
+	CHECK_FLAG(PIC_AR_64_27);
+	CHECK_FLAG(PIC_AR_256_135);
+#undef CHECK_FLAG
+}
 
-	const char *card = (argc > 1) ? argv[1] : "/dev/dri/card0";
+static void enumerateModeResources(int fd, const drmModeResPtr res) {
+	MSG("drmModeResPtr:");
+	MSG("\t.count_fbs = %d,", res->count_fbs);
+	MSG("\t.count_crtcs = %d,", res->count_crtcs);
+	MSG("\t.count_connectors = %d,", res->count_connectors);
+	MSG("\t.count_encoders = %d", res->count_encoders);
+	MSG("\t.width: %u..%u", res->min_width, res->max_width);
+	MSG("\t.height: %u..%u", res->min_height, res->max_height);
 
+	for (int i = 0; i < res->count_fbs; ++i)
+		MSG("\t.fbs[%d] = 0x%x", i, res->fbs[i]);
+
+	for (int i = 0; i < res->count_crtcs; ++i) {
+		drmModeCrtcPtr crtc = drmModeGetCrtc(fd, res->crtcs[i]);
+		if (!crtc)
+			continue;
+
+		MSGA("\t.crtcs[%d] = 0x%x: ", i, res->crtcs[i]);
+		MSGA("buffer_id=0x%x gamma_size=%d ", crtc->buffer_id, crtc->gamma_size);
+		MSGA("mode=(%u,%u %ux%u) mode_valid=%d ",
+			crtc->x, crtc->y, crtc->width, crtc->height, crtc->mode_valid);
+		MSG(" mode.name = %s", crtc->mode.name);
+		drmModeFreeCrtc(crtc);
+	}
+
+	for (int i = 0; i < res->count_connectors; ++i) {
+		//drmModeConnectorPtr conn = drmModeGetConnectorCurrent(fd, res->connectors[i]);
+		// No "Current" does EDID probe, etc.
+		drmModeConnectorPtr conn = drmModeGetConnector(fd, res->connectors[i]);
+		if (!conn)
+			continue;
+
+		MSGA("\t.connectors[%d] = 0x%x: ", i, res->connectors[i]);
+		MSG("encoder_id=0x%x type=%u type_id=0x%x connnection=%d mm=%ux%u subpixel=%d count_modes=%d count_props=%d count_encoders=%d",
+			conn->encoder_id, conn->connector_type, conn->connector_type_id, conn->connection, conn->mmWidth, conn->mmHeight,
+			conn->subpixel, conn->count_modes, conn->count_props, conn->count_encoders);
+
+		for (int j = 0; j < conn->count_modes; ++j) {
+			drmModeModeInfoPtr m = &conn->modes[j];
+			MSGA("\t\t.mode[%d] = %ux%u@%u .name=%s .clock=%ukHz .flags=(%x)",
+				j, m->hdisplay, m->vdisplay, m->vrefresh, m->name, m->clock, m->flags);
+			printDrmModeFlags(m->flags);
+			MSGA(".type=(%u)", m->type);
+			printDrmModeType(m->type);
+			MSG("");
+		}
+
+		drmModeFreeConnector(conn);
+	}
+
+	for (int i = 0; i < res->count_encoders; ++i) {
+		drmModeEncoderPtr enc = drmModeGetEncoder(fd, res->encoders[i]);
+		if (!enc)
+			continue;
+
+		MSG("\t.encoders[%d] = 0x%x: type=%u crtc_id=0x%x possible_crtcs=%x possible_clones=%x",
+			i, res->encoders[i],
+			enc->encoder_type, enc->crtc_id, enc->possible_crtcs, enc->possible_clones
+			);
+	}
+}
+
+void printVer(drmVersionPtr ver) {
+	if (!ver) {
+		MSG("ver=NULL");
+		return;
+	}
+
+	MSG("ver=%d.%d.%d driver=%.*s date=%.*s desc=%.*s",
+		ver->version_major, ver->version_minor, ver->version_patchlevel,
+		ver->name_len, ver->name,
+		ver->date_len, ver->date,
+		ver->desc_len, ver->desc);
+}
+
+typedef struct {
+	int fd;
+	drmVersionPtr ver, lib_ver;
+	drmModeResPtr mode_res;
+} DrmDevice;
+
+DrmDevice *ddOpen(const char *card) {
 	const int fd = open(card, O_RDONLY);
-	MSG("open = %d", fd);
-	if (fd < 2)
-		return 2;
+	DrmDevice dd = {.fd = fd};
+	if (fd < 2) {
+		MSG("Unable to open card \"%s\"", card);
+		return NULL;
+	}
+
+	MSG("%s = %d (is KMS=%d)", card, fd, drmIsKMS(fd));
 
 	drmSetClientCap(fd, DRM_CLIENT_CAP_UNIVERSAL_PLANES, 1);
+	drmSetClientCap(fd, DRM_CLIENT_CAP_ATOMIC, 1);
 
-	{
-		const drmVersionPtr ver = drmGetVersion(fd);
-		MSG("drm version %d.%d.%d driver=%.*s date=%.*s desc=%.*s",
-			ver->version_major, ver->version_minor, ver->version_patchlevel,
-			ver->name_len, ver->name,
-			ver->date_len, ver->date,
-			ver->desc_len, ver->desc);
-		drmFreeVersion(ver);
-	}
+	dd.ver = drmGetVersion(fd);
+	dd.lib_ver = drmGetLibVersion(fd);
 
-	{
-		const drmVersionPtr ver = drmGetLibVersion(fd);
-		MSG("drm lib version %d.%d.%d driver=%.*s date=%.*s desc=%.*s",
-			ver->version_major, ver->version_minor, ver->version_patchlevel,
-			ver->name_len, ver->name,
-			ver->date_len, ver->date,
-			ver->desc_len, ver->desc);
-		drmFreeVersion(ver);
-	}
+	printVer(dd.ver);
+	MSGA("lib "); printVer(dd.lib_ver);
 
 	{
 		const char *busid = drmGetBusid(fd);
@@ -90,12 +167,36 @@ int main(int argc, const char *argv[]) {
 		MSG("\tcount = %d", (int)stats.count);
 	}
 
-	drmModeResPtr res = drmModeGetResources(fd);
-	if (res) {
-		enumerateModeResources(fd, res);
-		drmModeFreeResources(res);
+	dd.mode_res = drmModeGetResources(fd);
+	if (dd.mode_res) {
+		enumerateModeResources(fd, dd.mode_res);
+	} else {
+		MSG("No mode resources?!");
 	}
 
+	DrmDevice *ret = malloc(sizeof(*ret));
+	memcpy(ret, &dd, sizeof(*ret));
+	return ret;
+}
+
+void ddClose(DrmDevice *dd) {
+	if (!dd)
+		return;
+
+	if (dd->mode_res)
+		drmModeFreeResources(dd->mode_res);
+
+	if (dd->lib_ver)
+		drmFreeVersion(dd->lib_ver);
+
+	if (dd->ver)
+		drmFreeVersion(dd->ver);
+
+	free(dd);
+}
+
+#if 0
+static void enumerateFbs(int fd) {
 #define MAX_FBS 16
 	uint32_t fbs[MAX_FBS];
 	int count_fbs = 0;
@@ -153,6 +254,24 @@ int main(int argc, const char *argv[]) {
 
 		drmModeFreeFB(fb);
 	}
+}
+#endif
 
-	close(fd);
+int main(int argc, const char *argv[]) {
+	const int available = drmAvailable();
+	if (!available) {
+		MSG("libdrm is not available");
+		return 1;
+	}
+
+	const char *card = (argc > 1) ? argv[1] : "/dev/dri/card0";
+
+	DrmDevice *const dd = ddOpen(card);
+	if (!dd)
+		return 2;
+
+	//enumerateFbs(dd->fd);
+
+	ddClose(dd);
+	return 0;
 }
