00085c346c376ab954770618228e6b0d9b6e1ea4
Ivan Avdeev <me@provod.works>
2024-09-02 00:01:43 -0400

    make build-rpi also upload and run binary on device

--- build-rpi.sh
+++ build-rpi.sh
@@ -4,9 +4,14 @@ set -eux
 #crossdev -t arm-linux-gnueabihf
 #rsync -vR --progress -rl --delete-after --safe-links pi@raspberry:/{usr,lib} cross-rootfs
 
+HOST="$1"
+PROG="$2"
+TARGET="build-rpi/rel/$PROG"
+
 export CC="arm-linux-gnueabihf-gcc"
 export CFLAGS="-I./cross-rootfs-sel/usr/include"
 #export LIBS="-Wl,-rpath-link,./cross-rootfs-sel/usr/lib/arm-linux-gnueabihf -L./cross-rootfs-sel/usr/lib/arm-linux-gnueabihf --sysroot=./cross-rootfs-sel -B./cross-rootfs-sel/usr/lib/arm-linux-gnueabihf"
 export LIBS="-Wl,-rpath-link,./cross-rootfs-sel/usr/lib/arm-linux-gnueabihf --sysroot=./cross-rootfs-sel -B./cross-rootfs-sel/usr/lib/arm-linux-gnueabihf"
 export BUILDDIR="build-rpi"
-make build-rpi/rel/enum
+
+make "$TARGET" && scp "$TARGET" "$HOST":~/tmp/ && ssh "$HOST" ~/tmp/"$PROG"
